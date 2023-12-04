a9aef2365fd0570761f5d91c3fc3d15439ad8e8a
Julien Kulbe <julien.kulbe@googlemail.com>
2023-12-04 18:38:46 +0100

    day 4 - task 2

--- advent_of_code_04/src/main.rs
+++ advent_of_code_04/src/main.rs
@@ -4,19 +4,10 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-const DEBUG: bool = true;
 const DEVELOP: bool = false;
 
-fn main() -> Result<()> {
-    let filename = if DEVELOP {
-        "input_small.txt"
-    } else {
-        "input.txt"
-    };
-    let file = File::open(filename)?;
-    let reader = BufReader::new(file);
-
-    let sum = reader.lines().flatten().fold(0, |acc, line| {
+fn task1(reader: BufReader<File>) -> u32 {
+    reader.lines().flatten().fold(0, |acc, line| {
         let (_, card) = line.split_once(':').unwrap();
         let (winning, current) = card.split_once('|').unwrap();
 
@@ -27,9 +18,81 @@ fn main() -> Result<()> {
             .count() as u32;
 
         acc + if count == 0 { 0 } else { 2_u32.pow(count - 1) }
-    });
+    })
+}
 
-    println!("Task 1: {sum}");
+struct Card {
+    winning: Vec<u32>,
+    current: Vec<u32>,
+    copies: u32,
+}
+
+impl Card {
+    fn new(content: String) -> Card {
+        let (_, card) = content.split_once(':').unwrap();
+        let (winning, current) = card.split_once('|').unwrap();
+
+        let winning = winning
+            .split_whitespace()
+            .map(|v| v.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+
+        let current = current
+            .split_whitespace()
+            .map(|v| v.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+
+        Card {
+            winning,
+            current,
+            copies: 1,
+        }
+    }
+
+    fn get_matching_cards(&self) -> usize {
+        self.current
+            .iter()
+            .filter(|c| self.winning.contains(c))
+            .count()
+    }
+
+    fn increase(&mut self, v: u32) {
+        self.copies += v;
+    }
+}
+
+fn task2(reader: BufReader<File>) -> u32 {
+    let mut cards = reader.lines().flatten().map(Card::new).collect::<Vec<_>>();
+
+    let winnings = cards
+        .iter()
+        .map(|c| c.get_matching_cards())
+        .collect::<Vec<_>>();
+
+    for (i, count) in winnings.iter().enumerate() {
+        let copies = cards[i].copies;
+        cards
+            .iter_mut()
+            .skip(i + 1)
+            .take(*count)
+            .for_each(|c| c.increase(copies));
+    }
+
+    cards.iter().map(|c| c.copies).sum()
+}
+
+fn main() -> Result<()> {
+    let filename = if DEVELOP {
+        "input_small.txt"
+    } else {
+        "input.txt"
+    };
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    //println!("Task 1: {}", task1(reader));
+
+    println!("Task 2: {}", task2(reader));
 
     Ok(())
 }
