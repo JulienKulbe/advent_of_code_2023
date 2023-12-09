1fcc9e005f308ace3dab8ee8a9b2ebf78d8b3496
Julien Kulbe <julien.kulbe@googlemail.com>
2023-12-09 16:10:28 +0100

    day 9 - task 2

--- advent_of_code_09/src/main.rs
+++ advent_of_code_09/src/main.rs
@@ -6,13 +6,8 @@ use std::{
 
 const DEVELOP: bool = false;
 
-fn main() -> Result<()> {
-    let filename = if DEVELOP {
-        "input_small.txt"
-    } else {
-        "input.txt"
-    };
-    let file = File::open(filename)?;
+fn task1(filename: &str) -> i32 {
+    let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
     let mut sum = 0;
@@ -46,7 +41,61 @@ fn main() -> Result<()> {
         sum += numbers[0].last().unwrap();
     }
 
-    println!("Task 1: {sum}\n");
+    sum
+}
+
+fn task2(filename: &str) -> i32 {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut sum = 0;
+    for line in reader.lines().flatten() {
+        let mut numbers = Vec::new();
+        numbers.push(
+            line.split_whitespace()
+                .map(|v| v.parse::<i32>().unwrap())
+                .collect::<Vec<_>>(),
+        );
+
+        for i in 0.. {
+            let mut next = Vec::new();
+            for j in 1..numbers[i].len() {
+                next.push(numbers[i][j] - numbers[i][j - 1]);
+            }
+            let all_zeros = next.iter().all(|v| *v == 0);
+            numbers.push(next);
+
+            if all_zeros {
+                break;
+            }
+        }
+
+        numbers.last_mut().unwrap().insert(0, 0);
+        for i in (1..numbers.len()).rev() {
+            let next = numbers[i - 1].first().unwrap() - numbers[i].first().unwrap();
+            numbers[i - 1].insert(0, next);
+        }
+
+        // println!("\n{line}");
+        // for number in &numbers {
+        //     println!("{number:?}");
+        // }
+
+        sum += numbers[0].first().unwrap();
+    }
+
+    sum
+}
+
+fn main() -> Result<()> {
+    let filename = if DEVELOP {
+        "input_small.txt"
+    } else {
+        "input.txt"
+    };
+
+    //println!("Task 1: {}\n", task1(filename));
+    println!("Task 2: {}\n", task2(filename));
 
     Ok(())
 }
