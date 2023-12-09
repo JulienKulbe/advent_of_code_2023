b04b81b9eb182947aa99226c69cc569814706050
Julien Kulbe <julien.kulbe@googlemail.com>
2023-12-09 16:56:53 +0100

    refactored

--- advent_of_code_09/src/main.rs
+++ advent_of_code_09/src/main.rs
@@ -1,12 +1,17 @@
-use anyhow::Result;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
 };
 
-const DEVELOP: bool = false;
+const DEVELOP: bool = true;
 
-fn task1(filename: &str) -> i32 {
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    First,
+    Last,
+}
+
+fn calculate(filename: &str, direction: Direction) -> i32 {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
@@ -32,70 +37,41 @@ fn task1(filename: &str) -> i32 {
             }
         }
 
-        numbers.last_mut().unwrap().push(0);
-        for i in (1..numbers.len()).rev() {
-            let next = numbers[i].last().unwrap() + numbers[i - 1].last().unwrap();
-            numbers[i - 1].push(next);
+        let last_row = numbers.last_mut().unwrap();
+        match direction {
+            Direction::Last => last_row.push(0),
+            Direction::First => last_row.insert(0, 0),
         }
 
-        sum += numbers[0].last().unwrap();
+        for i in (1..numbers.len()).rev() {
+            match direction {
+                Direction::Last => {
+                    let next = numbers[i].last().unwrap() + numbers[i - 1].last().unwrap();
+                    numbers[i - 1].push(next);
+                }
+                Direction::First => {
+                    let next = numbers[i - 1].first().unwrap() - numbers[i].first().unwrap();
+                    numbers[i - 1].insert(0, next);
+                }
+            }
+        }
+
+        sum += match direction {
+            Direction::Last => numbers[0].last().unwrap(),
+            Direction::First => numbers[0].first().unwrap(),
+        }
     }
 
     sum
 }
 
-fn task2(filename: &str) -> i32 {
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
-
-    let mut sum = 0;
-    for line in reader.lines().flatten() {
-        let mut numbers = Vec::new();
-        numbers.push(
-            line.split_whitespace()
-                .map(|v| v.parse::<i32>().unwrap())
-                .collect::<Vec<_>>(),
-        );
-
-        for i in 0.. {
-            let mut next = Vec::new();
-            for j in 1..numbers[i].len() {
-                next.push(numbers[i][j] - numbers[i][j - 1]);
-            }
-            let all_zeros = next.iter().all(|v| *v == 0);
-            numbers.push(next);
-
-            if all_zeros {
-                break;
-            }
-        }
-
-        numbers.last_mut().unwrap().insert(0, 0);
-        for i in (1..numbers.len()).rev() {
-            let next = numbers[i - 1].first().unwrap() - numbers[i].first().unwrap();
-            numbers[i - 1].insert(0, next);
-        }
-
-        // println!("\n{line}");
-        // for number in &numbers {
-        //     println!("{number:?}");
-        // }
-
-        sum += numbers[0].first().unwrap();
-    }
-
-    sum
-}
-
-fn main() -> Result<()> {
+fn main() {
     let filename = if DEVELOP {
         "input_small.txt"
     } else {
         "input.txt"
     };
 
-    //println!("Task 1: {}\n", task1(filename));
-    println!("Task 2: {}\n", task2(filename));
-
-    Ok(())
+    println!("Task 1: {}", calculate(filename, Direction::Last));
+    println!("Task 2: {}", calculate(filename, Direction::First));
 }
