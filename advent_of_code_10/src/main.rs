2a7c358c4cb406bc0c17dec59abc0f75e2eb8231
Julien Kulbe <julien.kulbe@googlemail.com>
2023-12-10 21:40:33 +0100

    day 10 - task 2

--- advent_of_code_10/src/main.rs
+++ advent_of_code_10/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use core::fmt;
 use std::fs::{self};
 
 const DEVELOP: bool = false;
@@ -25,43 +26,77 @@ struct Maze<'a> {
     heigth: usize,
 }
 
-impl<'a> Maze<'a> {
-    fn new(data: &'a [u8]) -> Self {
-        let width = data.iter().position(|&x| x == Literals::LF).unwrap();
-        let heigth = data.len() / (width + 1);
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+    Open,
+    Closed,
+    Path,
+    Loop,
+}
+
+struct FloodMap {
+    map: Vec<Vec<Tile>>,
+    width: usize,
+    heigth: usize,
+}
+
+impl FloodMap {
+    fn new(width: usize, heigth: usize) -> Self {
+        let mut map = Vec::new();
+        for _ in 0..heigth {
+            // create a row with: Closed | Path | Closed | Path | ... | Closed
+            let mut row = Vec::new();
+            for _ in 0..width {
+                row.push(Tile::Closed);
+                row.push(Tile::Path);
+            }
+            row.pop(); // remove last path
+            map.push(row);
+
+            // create a row with: Path | Path | Path | Path | ... | Path
+            map.push(vec![Tile::Path; 2 * width - 1]);
+        }
+        map.pop(); // remove last path row
 
         Self {
-            data,
-            width,
-            heigth,
+            map,
+            width: width * 2 - 1,
+            heigth: heigth * 2 - 1,
         }
     }
 
-    fn get_length(&self) -> usize {
-        // find starting position
-        let start = self.find_starting_position();
+    fn add_loop(&mut self, from: Position, to: Position) {
+        let from = (from.0 * 2, from.1 * 2);
+        let to = (to.0 * 2, to.1 * 2);
+        let over = ((from.0 + to.0) / 2, (from.1 + to.1) / 2);
 
-        // find all fields next to start
-        let neighbors = self.get_neighbors(start);
-
-        // calculate the route from all fields next to start to start again
-        neighbors
-            .iter()
-            .filter_map(|&node| self.get_path_length(start, node, start))
-            .max()
-            .unwrap()
+        self.map[from.1][from.0] = Tile::Loop;
+        self.map[over.1][over.0] = Tile::Loop;
+        self.map[to.1][to.0] = Tile::Loop;
     }
 
-    fn find_starting_position(&self) -> Position {
-        let index = self
-            .data
-            .iter()
-            .position(|&x| x == Literals::START)
-            .unwrap();
+    fn flood_fill_map(&mut self) {
+        for x in 0..self.width {
+            self.flood_fill((x, 0));
+            self.flood_fill((x, self.heigth - 1));
+        }
+        for y in 0..self.heigth {
+            self.flood_fill((0, y));
+            self.flood_fill((self.width - 1, y));
+        }
+    }
 
-        let x = index % (self.width + 1);
-        let y = index / (self.width + 1);
-        (x, y)
+    fn flood_fill(&mut self, start: Position) {
+        if self.map[start.1][start.0] != Tile::Closed && self.map[start.1][start.0] != Tile::Path {
+            return;
+        }
+
+        self.map[start.1][start.0] = Tile::Open;
+
+        let neighbors = self.get_neighbors(start);
+        for neighbor in neighbors {
+            self.flood_fill(neighbor);
+        }
     }
 
     fn get_neighbors(&self, current: Position) -> Vec<Position> {
@@ -79,6 +114,116 @@ impl<'a> Maze<'a> {
             neighbors.push((current.0, current.1 + 1));
         }
         neighbors
+    }
+
+    fn get_closed_tiles(&self) -> usize {
+        let mut sum = 0;
+        for row in self.map.iter() {
+            for tile in row {
+                if *tile == Tile::Closed {
+                    sum += 1;
+                }
+            }
+        }
+        sum
+    }
+}
+
+impl fmt::Display for FloodMap {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in self.map.iter() {
+            for tail in row {
+                let c = match tail {
+                    Tile::Closed => 'I',
+                    Tile::Open => 'O',
+                    Tile::Loop => '=',
+                    Tile::Path => ' ',
+                };
+                write!(f, "{c}")?
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+impl<'a> Maze<'a> {
+    fn new(data: &'a [u8]) -> Self {
+        let width = data.iter().position(|&x| x == Literals::LF).unwrap();
+        let heigth = data.len() / (width + 1);
+
+        Self {
+            data,
+            width,
+            heigth,
+        }
+    }
+
+    fn get_length(&self) -> usize {
+        let start = self.find_starting_position();
+
+        if DEVELOP {
+            // find all fields next to start
+            let neighbors = self.get_neighbors(start);
+
+            // calculate the route from all fields next to start to start again
+            neighbors
+                .iter()
+                .filter_map(|&node| self.get_path_length(start, node, start))
+                .max()
+                .unwrap()
+        } else {
+            let next = (start.0, start.1 + 1);
+            self.get_path_length(start, next, start).unwrap()
+        }
+    }
+
+    fn get_enclosed_tiles(&self) -> usize {
+        // create open/ enclosed map
+        let mut map = FloodMap::new(self.width, self.heigth);
+        //println!("Created: \n{map}");
+
+        // creat path and mark tiles
+        let start = self.find_starting_position();
+        let next = (start.0, start.1 + 1);
+        self.mark_path(&mut map, start, next, start);
+        //println!("Mark Path:\n{map}");
+
+        // flood fill from edge of grid
+        map.flood_fill_map();
+        //println!("Flood fill:\n{map}");
+
+        // search all enclosed tiles
+        map.get_closed_tiles()
+    }
+
+    fn get_neighbors(&self, current: Position) -> Vec<Position> {
+        let mut neighbors = Vec::new();
+        if current.0 > 0 {
+            neighbors.push((current.0 - 1, current.1));
+        }
+        if current.0 < self.width - 1 {
+            neighbors.push((current.0 + 1, current.1));
+        }
+        if current.1 > 0 {
+            neighbors.push((current.0, current.1 - 1));
+        }
+        if current.1 < self.heigth - 1 {
+            neighbors.push((current.0, current.1 + 1));
+        }
+        neighbors
+    }
+
+    fn find_starting_position(&self) -> Position {
+        let index = self
+            .data
+            .iter()
+            .position(|&x| x == Literals::START)
+            .unwrap();
+
+        let x = index % (self.width + 1);
+        let y = index / (self.width + 1);
+        (x, y)
     }
 
     fn get_next_field(&self, previous: Position, current: Position) -> Option<Position> {
@@ -120,14 +265,32 @@ impl<'a> Maze<'a> {
         }
     }
 
+    fn mark_path(
+        &self,
+        map: &mut FloodMap,
+        mut previous: Position,
+        mut current: Position,
+        end: Position,
+    ) {
+        map.add_loop(previous, current);
+        while current != end {
+            match self.get_next_field(previous, current) {
+                Some(next) => {
+                    previous = current;
+                    current = next;
+                    map.add_loop(previous, current);
+                }
+                None => panic!("Invalid path"),
+            }
+        }
+    }
+
     fn get_path_length(
         &self,
         mut previous: Position,
         mut current: Position,
         end: Position,
     ) -> Option<usize> {
-        //println!("{previous:?} -> {current:?}");
-
         for i in 0.. {
             if current == end {
                 return Some(i);
@@ -136,7 +299,6 @@ impl<'a> Maze<'a> {
                 Some(next) => {
                     previous = current;
                     current = next;
-                    //println!("-> {next:?}");
                 }
                 None => {
                     return None;
@@ -149,7 +311,7 @@ impl<'a> Maze<'a> {
 
 fn main() -> Result<()> {
     let filename = if DEVELOP {
-        "input_small.txt"
+        "input_small4.txt"
     } else {
         "input.txt"
     };
@@ -158,6 +320,7 @@ fn main() -> Result<()> {
     let maze = Maze::new(contents.as_bytes());
 
     println!("Task 1: {}", (maze.get_length() + 1) / 2);
+    println!("Task 2: {}", maze.get_enclosed_tiles());
 
     Ok(())
 }
