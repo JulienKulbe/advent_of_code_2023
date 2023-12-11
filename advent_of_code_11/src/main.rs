98963a51206a9541090ef7a968a23150719a5585
Julien Kulbe <julien.kulbe@googlemail.com>
2023-12-11 17:43:30 +0100

    day 11 - task 2

--- advent_of_code_11/src/main.rs
+++ advent_of_code_11/src/main.rs
@@ -6,6 +6,9 @@ use std::{
 
 const DEVELOP: bool = false;
 
+const MULTIPLY: usize = 1_000_000;
+
+#[derive(Debug)]
 struct Galaxy {
     x: usize,
     y: usize,
@@ -41,29 +44,28 @@ fn main() -> Result<()> {
     }
 
     // found rows and columns without any galaxies
-    let rows = (0..heigth)
+    let rows = (0..=heigth)
         .filter(|&y| galaxies.iter().all(|g| g.y != y))
         .collect::<Vec<_>>();
-    let columns = (0..width)
+    let columns = (0..=width)
         .filter(|&x| galaxies.iter().all(|g| g.x != x))
         .collect::<Vec<_>>();
 
     // correct galaxy coordinates
     for galaxy in &mut galaxies {
-        galaxy.y += rows.iter().filter(|&&r| r < galaxy.y).count();
-        galaxy.x += columns.iter().filter(|&&c| c < galaxy.x).count();
+        galaxy.y += (MULTIPLY - 1) * rows.iter().filter(|&&r| r < galaxy.y).count();
+        galaxy.x += (MULTIPLY - 1) * columns.iter().filter(|&&c| c < galaxy.x).count();
     }
 
     // calculate manhattan distance of each pair
     let mut distance = 0;
     for (index, galaxy_1) in galaxies.iter().enumerate() {
         for galaxy_2 in galaxies.iter().skip(index) {
-            let d = galaxy_1.x.abs_diff(galaxy_2.x) + galaxy_1.y.abs_diff(galaxy_2.y);
-            distance += d;
+            distance += galaxy_1.x.abs_diff(galaxy_2.x) + galaxy_1.y.abs_diff(galaxy_2.y);
         }
     }
 
-    println!("Task 1: {distance}");
+    println!("Task: {distance}");
 
     Ok(())
 }
