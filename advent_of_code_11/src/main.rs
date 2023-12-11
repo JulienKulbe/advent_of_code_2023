f45f21563a2467a46024c39bc370857b6349d686
Julien Kulbe <julien.kulbe@googlemail.com>
2023-12-11 17:23:37 +0100

    day 11 - task 1

---
+++ advent_of_code_11/src/main.rs
@@ -0,0 +1,69 @@
+use anyhow::Result;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+const DEVELOP: bool = false;
+
+struct Galaxy {
+    x: usize,
+    y: usize,
+}
+
+impl Galaxy {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+fn main() -> Result<()> {
+    let filename = if DEVELOP {
+        "input_small.txt"
+    } else {
+        "input.txt"
+    };
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    // create vec with all galaxy coordinates
+    let mut galaxies = Vec::new();
+    let mut width = 0;
+    let mut heigth = 0;
+    for (y, line) in reader.lines().flatten().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '#' {
+                galaxies.push(Galaxy::new(x, y));
+            }
+            width = x;
+        }
+        heigth = y;
+    }
+
+    // found rows and columns without any galaxies
+    let rows = (0..heigth)
+        .filter(|&y| galaxies.iter().all(|g| g.y != y))
+        .collect::<Vec<_>>();
+    let columns = (0..width)
+        .filter(|&x| galaxies.iter().all(|g| g.x != x))
+        .collect::<Vec<_>>();
+
+    // correct galaxy coordinates
+    for galaxy in &mut galaxies {
+        galaxy.y += rows.iter().filter(|&&r| r < galaxy.y).count();
+        galaxy.x += columns.iter().filter(|&&c| c < galaxy.x).count();
+    }
+
+    // calculate manhattan distance of each pair
+    let mut distance = 0;
+    for (index, galaxy_1) in galaxies.iter().enumerate() {
+        for galaxy_2 in galaxies.iter().skip(index) {
+            let d = galaxy_1.x.abs_diff(galaxy_2.x) + galaxy_1.y.abs_diff(galaxy_2.y);
+            distance += d;
+        }
+    }
+
+    println!("Task 1: {distance}");
+
+    Ok(())
+}
