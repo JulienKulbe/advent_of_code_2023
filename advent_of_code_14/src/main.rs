14077bc3b8cda90cb9005e2f948c81d66a871e36
Julien Kulbe <julien.kulbe@googlemail.com>
2023-12-15 06:33:41 +0100

    day 14 - task 2 (not finished)

--- advent_of_code_14/src/main.rs
+++ advent_of_code_14/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-const DEVELOP: bool = true;
+const DEVELOP: bool = false;
 
 fn main() -> Result<()> {
     let filename = if DEVELOP {
@@ -13,23 +13,73 @@ fn main() -> Result<()> {
         "input.txt"
     };
 
-    let file = File::open(filename)?;
+    {
+        let mut lines = create_map(filename);
+        tilt_north(&mut lines);
+        println!("Task 1: {}", calculate_load(&lines));
+    }
+    {
+        let mut lines = create_map(filename);
+
+        let mut history = Vec::new();
+        for i in 1..200 {
+            tilt_north(&mut lines);
+            tilt_west(&mut lines);
+            tilt_south(&mut lines);
+            tilt_east(&mut lines);
+
+            if i > 161 && i < 180 {
+                println!("{i}: {}", calculate_load(&lines));
+            }
+            // if i < 10 {
+            //     //print_map(&lines);
+            //     println!("{i}: {}", calculate_load(&lines));
+            // }
+
+            let current = lines.clone();
+            for (j, previous) in history.iter().enumerate() {
+                if *previous == current {
+                    println!("found duplicate {j} --> {i} ");
+                    break;
+                }
+            }
+            history.push(current);
+        }
+
+        // (i-10) % 7 + 2
+        // (1000000000 - 10) % 7 + 2 = 5
+
+        // (i - 180) % 18 + 162
+        // (1000000000 - 180) % 18 + 162 = 172
+
+        println!("Task 2: {}", calculate_load(&lines));
+    }
+
+    Ok(())
+}
+
+fn print_map(lines: &Vec<Vec<char>>) {
+    println!();
+    for line in lines {
+        println!("{line:?}");
+    }
+}
+
+fn create_map(filename: &str) -> Vec<Vec<char>> {
+    let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut lines = reader
+    reader
         .lines()
         .flatten()
         .map(|line| line.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
+fn tilt_north(lines: &mut Vec<Vec<char>>) {
     let width = lines[0].len();
     let heigth = lines.len();
 
-    // for line in lines.iter() {
-    //     println!("{line:?}");
-    // }
-
-    let mut sum = 0;
     for x in 0..width {
         let mut next_pos: Option<usize> = None;
         let mut y = 0;
@@ -56,20 +106,129 @@ fn main() -> Result<()> {
             }
             y += 1;
         }
+    }
+}
 
+fn tilt_south(lines: &mut Vec<Vec<char>>) {
+    let width = lines[0].len();
+    let heigth = lines.len();
+
+    for x in 0..width {
+        let mut next_pos: Option<usize> = None;
+        let mut y = heigth - 1;
+
+        loop {
+            match lines[y][x] {
+                'O' => {
+                    if let Some(next) = next_pos {
+                        lines[next][x] = 'O';
+                        lines[y][x] = '.';
+                        next_pos = None;
+                        y = next;
+                    }
+                }
+                '#' => {
+                    next_pos = None;
+                }
+                '.' => {
+                    if next_pos.is_none() {
+                        next_pos = Some(y);
+                    }
+                }
+                _ => panic!("invalid token"),
+            }
+
+            if y == 0 {
+                break;
+            } else {
+                y -= 1;
+            }
+        }
+    }
+}
+
+fn tilt_west(lines: &mut Vec<Vec<char>>) {
+    let width = lines[0].len();
+    let heigth = lines.len();
+
+    for y in 0..heigth {
+        let mut next_pos: Option<usize> = None;
+        let mut x = 0;
+
+        while x < width {
+            match lines[y][x] {
+                'O' => {
+                    if let Some(next) = next_pos {
+                        lines[y][next] = 'O';
+                        lines[y][x] = '.';
+                        next_pos = None;
+                        x = next;
+                    }
+                }
+                '#' => {
+                    next_pos = None;
+                }
+                '.' => {
+                    if next_pos.is_none() {
+                        next_pos = Some(x);
+                    }
+                }
+                _ => panic!("invalid token"),
+            }
+            x += 1;
+        }
+    }
+}
+
+fn tilt_east(lines: &mut Vec<Vec<char>>) {
+    let width = lines[0].len();
+    let heigth = lines.len();
+
+    for y in 0..heigth {
+        let mut next_pos: Option<usize> = None;
+        let mut x = width - 1;
+
+        loop {
+            match lines[y][x] {
+                'O' => {
+                    if let Some(next) = next_pos {
+                        lines[y][next] = 'O';
+                        lines[y][x] = '.';
+                        next_pos = None;
+                        x = next;
+                    }
+                }
+                '#' => {
+                    next_pos = None;
+                }
+                '.' => {
+                    if next_pos.is_none() {
+                        next_pos = Some(x);
+                    }
+                }
+                _ => panic!("invalid token"),
+            }
+
+            if x == 0 {
+                break;
+            } else {
+                x -= 1;
+            }
+        }
+    }
+}
+
+fn calculate_load(lines: &Vec<Vec<char>>) -> usize {
+    let width = lines[0].len();
+    let heigth = lines.len();
+
+    let mut sum = 0;
+    for x in 0..width {
         for y in 0..heigth {
             if lines[y][x] == 'O' {
                 sum += heigth - y;
             }
         }
     }
-
-    // println!();
-    // for line in lines.iter() {
-    //     println!("{line:?}");
-    // }
-
-    println!("Task 1: {sum}");
-
-    Ok(())
+    sum
 }
